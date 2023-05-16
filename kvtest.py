4e2dc42581be15d42947a665577e438721367ce0
Nikolai <9651027@mail.ru>
2023-05-16 11:54:16 +0300

    импортировал math вычислил корни уравнения

--- kvtest.py
+++ kvtest.py
@@ -1,3 +1,4 @@
+import math
 baddata = True
 while baddata == True:
     try:
@@ -12,7 +13,11 @@ D= (b*b)-(4*a*c)
 
 if D>0:
     print('Два корня')
+    d = math.sqrt(D)
+    X1= ((-b)+d)/(2*a)
+    X2 = ((-b) - d) / (2 * a)
 elif D==0:
     print('Один корень')
+    X1 = (-b) / (2 * a)
 else:
     print('Нет корней')
