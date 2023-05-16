777f2f95614e52837f3aa920a378b5833caf44bd
Nikolai <9651027@mail.ru>
2023-05-16 11:45:54 +0300

    вычислил дискриминант. Написал проверку условий

--- kvtest.py
+++ kvtest.py
@@ -8,3 +8,11 @@ while baddata == True:
     except:
         print('Не удалось получить данные!')
 
+D= (b*b)-(4*a*c)
+
+if D>0:
+    print('Два корня')
+elif D==0:
+    print('Один корень')
+else:
+    print('Нет корней')
